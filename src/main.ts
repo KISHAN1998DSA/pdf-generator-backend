00c9d78db12236945536f0df383a53983cee45f9
Kishan Thakur <kishanthakurdsa1998@gmail.com>
2025-07-26 09:46:24 +0530

    fix build issue

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://pdf-generator-livid.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
